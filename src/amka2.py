6cc42fdd9f2cbfeddb0db0274ef60464b0d865bf
cjswldn99@gmail.com <cjswldn99@gmail.com>
2022-06-03 15:16:58 +0900

    2step fine-tuning Window vr.

--- src/amka2.py
+++ src/amka2.py
@@ -2,14 +2,17 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras import models, layers, optimizers
 import matplotlib.pyplot as plt
+import numpy as np
 
-INPUT_SHAPE = (128, 128, 3)
+INPUT_SHAPE = (64, 64, 3)
+TARGET_SIZE = (64, 64)
+EPOCHS = 100
 PATH_FIGURE = ""
 PATH_MODELS = ""
-WINDOW_PATH = ""
+WINDOW_PATH = "C:/Users/cjswl/Desktop/amerry_vs_kano_data/"
 
 
-def data_generator(directory, target_size=(128,128), batch_size=20, class_mode='binary'):
+def data_generator(directory, target_size=TARGET_SIZE, batch_size=20, class_mode='binary'):
     datagen = ImageDataGenerator(rescale=1./255)
     return datagen.flow_from_directory(
         directory,
@@ -44,35 +47,37 @@ def pre_training(train_data, val_data, test_data):
     )
 
     pre_history = model.fit(
-        train_data, epochs = 300, validation_data = val_data
+        train_data, epochs = EPOCHS//2, validation_data = val_data
     )
-    plot_history(pre_history, history_type="loss")
-    plot_history(pre_history, history_type="acc")
+    plot_history(pre_history, title="pre_train_loss.jpg", history_type="loss")
+    plot_history(pre_history, title="pre_train_acc.jpg",history_type="accuracy")
     test_loss, test_acc = model.evaluate(test_data)
     results = dict_result(
-        pre_history.history["train_loss"], pre_history.history["train_acc"],
+        pre_history.history["loss"][-1], pre_history.history["accuracy"][-1],
         test_loss, test_acc
     )
     save_txt(results)
     model.save("amka2_pretraining.h5")
 
 def fine_tuning(train_data, val_data, test_data):
-    model = models.load("amka2_pretraining.h5")
+    model = models.load_model("amka2_pretraining.h5")
     conv_base = model.layers[0]
 
     for layer in conv_base.layers:
-        if layer.name.startwith("block5"):
+        if layer.name.startswith("block5"):
             layer.trainable = True
     model.compile(
         optimizer = optimizers.RMSprop(learning_rate=1e-5),
         loss = "binary_crossentropy", metrics = ["accuracy"]
     )
     history = model.fit(
-        train_data, epochs = 300, validation_data = val_data
+        train_data, epochs = EPOCHS, validation_data = val_data
     )
+    plot_history(history, title="fine_tuning_loss", history_type="loss")
+    plot_history(history, title="fine_tuning_acc",history_type="accuracy")
     test_loss, test_acc = model.evaluate(test_data)
     results = dict_result(
-        history.history["train_loss"], history.history["train_acc"],
+        history.history["loss"][-1], history.history["accuracy"][-1],
         test_loss, test_acc
     )
     save_txt(results)
@@ -104,6 +109,8 @@ def plot_history(history, title="amka2_loss", history_type="loss"):
     plt.ylabel(history_type)
     plt.xlabel("Epochs")
     plt.legend(["Training", "Validation"], loc=0)
+    plt.savefig(title)
+    plt.clf()
 
 def save_txt(result = {}, title="amka2_result"):
     key_list = ["train_loss", "train_acc", "test_loss", "test_acc"]
@@ -120,9 +127,9 @@ def save_txt(result = {}, title="amka2_result"):
             f.write(string)
 
 def main():
-    train_data = data_generator()
-    val_data = data_generator()
-    test_data = data_generator()
+    train_data = data_generator(WINDOW_PATH + "project_train")
+    val_data = data_generator(WINDOW_PATH + "project_val")
+    test_data = data_generator(WINDOW_PATH + "project_test")
 
     pre_training(train_data, val_data, test_data)
     fine_tuning(train_data, val_data, test_data)
